6a3e4c399c090e1808ad697ae40327c98157f836
LauraMayaVayaHayaRaya <lauramaya86@gmail.com>
2019-06-26 18:49:56 -0500

    se enlaza con json

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+const showData = () => {
+  
   return 'example';
 };
 
--- src/main.js
+++ src/main.js
@@ -3,3 +3,15 @@ const showList=()=>{
 };
 
 document.getElementById("categories").addEventListener("click", showList);
+
+
+let charactersApi = "https://rickandmortyapi.com/api/character/";
+let request = new XMLHttpRequest();
+request.open("GET", charactersApi);
+request.responseType = "json";
+request.send();
+request.onload = function (){
+    let characters = request.response;
+    
+    console.log(characters);
+}
