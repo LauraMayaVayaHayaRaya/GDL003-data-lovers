d71da11f3cc104fb44f502272b0d33eb385a2d86
pahua <dadiante@gmail.com>
2019-07-03 16:46:37 -0500

    se comenta la función fetch para comenzar a usar el js

--- src/data.js
+++ src/data.js
@@ -1,16 +1,37 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const showData = () => {
-  
-  
-};
+window.datalovers={
+  filterData:
+  (searchBoxValue, showApi) => {
+    console.log(searchBoxValue.value);
+    console.log(showApi);
+  }
+}
+  //acceder a dom
+  //escribir función de filtrado
+  //enlazar value de text a función
+  //retornar Información en consola
+  //retornar información en web
+//var newArray = arr.filter(callback(currentValue[, index[, array]])[, thisArg])
 
-const filtrado =() => {
-  return 'filtrado';
-};
 
-window.filtrado = {
-  filtrado,
+
+
+
+
+
+
+/*
+  return 'filterData';
+};
+}
+
+window.filterData = {
+  filterData,
   example
 };
+*/
+//las otras funciones serían:
+// sortData(data, sortBy, sortOrder)
+// computeStats(data)
