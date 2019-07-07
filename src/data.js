c2c6f6b22a849b934ad6e88aa831da76c7a452bd
pahua <dadiante@gmail.com>
2019-07-06 23:18:13 -0500

    se reacomodan elementos perdidos en merge

--- src/data.js
+++ src/data.js
@@ -24,12 +24,15 @@ const sort = (key, order='asc') => {
       (comparison * -1) : comparison
     );
   };
-},
+
+};
 
 const filter =(results,specie)=>{
   let characters=results.filter(character => character.species == specie);
   return characters;
-};
+}
+
+
 
 window.sort=sort;
-window.filter=filter;
+window.filter =filter;
