fd5b75a76349e4c7a73b84a04c4cf1bdef674d3e
pahua <dadiante@gmail.com>
2019-07-07 23:37:27 -0500

    se agrega función de cálculo de estadísticas

--- src/main.js
+++ src/main.js
@@ -3,18 +3,22 @@ const showList=(results=false)=>{
     RICKANDMORTY.results=results;
   }
   document.getElementById("categories").style.display="none";
+  document.getElementById("statisticsSection").style.display="none";
+
 
 document.getElementById("resultItems").innerHTML='';
   Object.keys(RICKANDMORTY.results).forEach(function(item){
     let character=RICKANDMORTY.results[item];
+
+    console.log(character);
     let htmlElement=document.createElement('section');
     htmlElement.className='category';
-
     htmlElement.innerHTML='<a href="#characters">'+character.name+'</a><img src="'+character.image+'">';
     document.getElementById("resultItems").appendChild(htmlElement);
 
   });
   document.getElementById("result").style.display="block";
+  document.getElementById("statisticsSection").style.display="none";
 };
 document.getElementById("characters").addEventListener("click", function(){showList()});
 
@@ -22,15 +26,17 @@ document.getElementById("characters").addEventListener("click", function(){showL
 const showHome=()=>{
   document.getElementById("result").style.display="none";
   document.getElementById("categories").style.display="block";
-};
+  document.getElementById("statisticsSection").style.display="none";
 
+};
 document.getElementById("homeButton").addEventListener("click", showHome);
+
+
 
 const sortList=(order='asc')=>{
   RICKANDMORTY.results.sort(sort('name',order));
   showList(RICKANDMORTY.results);
 };
-
 document.getElementById('sortAsc').addEventListener('click',function(){sortList()});
 document.getElementById('sortDesc').addEventListener('click',function(){sortList('desc')});
 
@@ -40,6 +46,51 @@ document.getElementById('sortDesc').addEventListener('click',function(){sortList
     let specie=document.querySelectorAll("input[name=species]:checked")[0].value;
     let results=filter(characters,specie);
     showList(results);
-
   };
   document.getElementById("filterButton").addEventListener("click", filterData);
+
+const showStatistics = () => {
+  document.getElementById("result").style.display="block";
+  document.getElementById("sortAsc").style.display="none";
+  document.getElementById("sortDesc").style.display="none";
+  document.getElementById("categories").style.display="none";
+  //document.getElementById("statisticsSection").style.display="block";
+  document.getElementById("titleSort").innerHTML="Select a button to calculate";
+  document.getElementById("titleFilter").style.display="none";
+  document.getElementById("titleCharacters").innerHTML="STATISTICS";
+  document.getElementById("statsButton").style.display="block";
+  document.getElementById("filterButton").style.display="none";
+  };
+    document.getElementById("statisticsButtonImg").addEventListener("click", showStatistics);
+
+const calculateStatistics = () => {
+    let characters=RICKANDMORTY.results;
+    let specieSelected=document.querySelectorAll("input[name=species]:checked")[0].value;
+    let specieSelectedList=filter(characters,specieSelected);
+    let percentage = parseFloat(specieSelectedList.length*100 /493).toFixed(2);
+    document.getElementById("totalStats").innerHTML=("Total of " + specieSelected + "s"  + " = " + specieSelectedList.length + ".");
+    document.getElementById("percentageStats").innerHTML=("The specie "+ specieSelected + " represents " + percentage + "% " + "of the 493 characters of the serie");
+  };
+document.getElementById("statsButton").addEventListener("click", calculateStatistics);
+
+
+  /* const filtrado = () => {
+     let radioForm = document.getElementById("filterFormHtml");
+     radioForm.addEventListener("submit", function(event) {
+       let data = new FormData(radioForm);
+       let radioValue = "";
+       for (const entry of data) {
+                 radioValue = entry[1];
+       };
+
+       let rickandmortylist = RICKANDMORTY.results;
+       console.log (rickandmortylist);
+       console.log(radioValue);
+
+       let filtered = rickandmortylist.filter(function(el) {
+           return el.species == radioValue;
+         })
+       console.log(filtered);
+       event.preventDefault();
+     }, false);
+   }*/
